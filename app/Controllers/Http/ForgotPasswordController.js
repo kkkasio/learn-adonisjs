a062f2f402237a3f3bd518fd9a9cf412b9e6f180
Kasio Eduardo <kasioeduardo13@gmail.com>
2019-11-10 20:37:18 -0300

    Enviando e-mail

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -1,8 +1,11 @@
 'use strict'
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+
 const crypto = require('crypto')
 const User = use('App/Models/User')
+/** @type {typeof import('@adonisjs/mail/src/Mail')} */
+const Main = use('Mail')
 
 class ForgotPasswordController {
   async store ({ request, response }) {
@@ -14,6 +17,21 @@ class ForgotPasswordController {
       user.token_created_at = new Date()
 
       await user.save()
+
+      await Main.send(
+        ['emails.forgot_password'],
+        {
+          email,
+          token: user.token,
+          link: `${request.input('redirect_url')}?token=${user.token}`
+        },
+        message => {
+          message
+            .to(user.email)
+            .from('kasio@gmail.com', 'Kásio Eduardo | MinhaAPI')
+            .subject('Recuperação de Senha')
+        }
+      )
     } catch (error) {
       return response
         .status(error.status)
