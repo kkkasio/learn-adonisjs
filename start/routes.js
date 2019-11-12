84feba295d57f9b4d2b9f316d1a2b1b25ca68e64
Kasio Eduardo <kasioeduardo13@gmail.com>
2019-11-12 12:40:07 -0300

    Upload de arquivo

--- start/routes.js
+++ start/routes.js
@@ -14,14 +14,16 @@
 */
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
-const Route = use('Route')
+const Route = use('Route');
 
 Route.get('/', () => {
-  return { version: '1.0.0' }
-})
+  return { version: '1.0.0' };
+});
 
-Route.post('users', 'UserController.store')
-Route.post('sessions', 'SessionController.store')
+Route.post('users', 'UserController.store');
+Route.post('sessions', 'SessionController.store');
 
-Route.post('password', 'ForgotPasswordController.store')
-Route.put('password', 'ForgotPasswordController.update')
+Route.post('password', 'ForgotPasswordController.store');
+Route.put('password', 'ForgotPasswordController.update');
+
+Route.post('files', 'FileController.store');
