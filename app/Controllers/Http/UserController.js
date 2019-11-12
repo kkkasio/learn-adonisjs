dd29ea0bb19f0c01e40c8d734a3dbd2bdfb45b0c
Kasio Eduardo <kasioeduardo13@gmail.com>
2019-11-12 16:22:32 -0300

    Lidando com excessões

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,16 +1,16 @@
-'use strict'
+'use strict';
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const User = use('App/Models/User')
+const User = use('App/Models/User');
 
 class UserController {
-  async store ({ request }) {
-    const data = request.only(['username', 'email', 'password'])
+  async store({ request }) {
+    const data = request.only(['username', 'email', 'password']);
 
-    const user = await User.create(data)
+    const user = await User.create(data);
 
-    return user
+    return user;
   }
 }
 
-module.exports = UserController
+module.exports = UserController;
