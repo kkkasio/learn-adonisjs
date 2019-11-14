44669ef4413d325a57bb6dd8f9c09737b0be3745
Kasio <kasioeduardo13@gmail.com>
2019-11-13 23:48:20 -0300

    Internacionalização/Validação

--- app/Validators/ForgotPassword.js
+++ app/Validators/ForgotPassword.js
@@ -1,8 +1,14 @@
 'use strict';
 
+const Antl = use('Antl');
+
 class ForgotPassword {
   get validateAll() {
     return true;
+  }
+
+  get messages() {
+    return Antl.list('validation');
   }
 
   get rules() {
